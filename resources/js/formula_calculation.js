cd10153491e63f1e834d9c070393c1dad2572149
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-13 09:42:11 +0300

    fix alert

--- resources/js/formula_calculation.js
+++ resources/js/formula_calculation.js
@@ -3,16 +3,19 @@ function calculateRectangle() {
     let length = parseFloat(document.getElementById("length").value);
     let width = parseFloat(document.getElementById("width").value);
 
-
+    let error = "ведіть значення"
     let perimeter = 2 * (length + width);
     let area = length * width;
     let diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
 
-    if ( length <= 0 || width <= 0) {
-        alert("Будь ласка, введіть додатні числа для довжини та ширини.");
-        return;
+    if (isNaN(length) || isNaN(width) || length <= 0 || width <= 0) {
+        document.getElementById("perimeter").textContent = error.toString();
+        document.getElementById("area").textContent = error.toString();
+        document.getElementById("diagonal").textContent = error.toString();
+    }else {
+        document.getElementById("perimeter").textContent = perimeter.toString();
+        document.getElementById("area").textContent = area.toString();
+        document.getElementById("diagonal").textContent = diagonal.toString();
     }
-    document.getElementById("perimeter").textContent = perimeter.toString();
-    document.getElementById("area").textContent = area.toString();
-    document.getElementById("diagonal").textContent = diagonal.toString();
+
 }
